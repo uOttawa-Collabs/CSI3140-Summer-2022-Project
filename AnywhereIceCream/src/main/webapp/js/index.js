be3fc3583e8c75e46166753e45af5373b6be9fe8
Xiayu Li <71458184+SayanoSong@users.noreply.github.com>
2022-07-18 23:10:16 -0400

    SignIn/UP Implement (#5)
    * Change
    
    * Refactor with JQuery
    
    ...and removed global event handler, which would cause problems
    
    Signed-off-by: wxx9248 <wxx9248@qq.com>
    
    * Clear input after button click
    
    Signed-off-by: wxx9248 <wxx9248@qq.com>
    
    Co-authored-by: wxx9248 <wxx9248@qq.com>

--- AnywhereIceCream/src/main/webapp/js/index.js
+++ AnywhereIceCream/src/main/webapp/js/index.js
@@ -1,2 +1,46 @@
-(() => {
-})();
+$(document).ready(() => {
+    let signModal = $("#nav-modal");
+    let signTitle = $("#sign-title");
+    let modalClose = $("#modal-close");
+    let modalSubmit = $("#modal-submit");
+
+    let inputEmail = $("input#email");
+    let inputPassword = $("input#pwd");
+
+    let signAction = (text) => {
+        alert(text + " successfully");
+        signModal.css("display", "none");
+    };
+
+    $("button#navbar-button-sign-in").click(() => {
+        inputEmail.val("");
+        inputPassword.val("");
+        signTitle.text("Sign In");
+        modalSubmit.text("Sign In");
+        modalSubmit.off("click");
+        modalSubmit.click((e) => {
+            e.preventDefault();
+            signAction("Signed in");
+        });
+
+        signModal.css("display", "block");
+    });
+
+    $("button#navbar-button-sign-up").click(() => {
+        inputEmail.val("");
+        inputPassword.val("");
+        signTitle.text("Sign Up");
+        modalSubmit.text("Sign Up");
+        modalSubmit.off("click");
+        modalSubmit.click((e) => {
+            e.preventDefault();
+            signAction("Signed up");
+        });
+
+        signModal.css("display", "block");
+    });
+
+    modalClose.click(() => {
+        $("#nav-modal").css("display", "none");
+    });
+});
