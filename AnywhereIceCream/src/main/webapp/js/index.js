3d98a87b03e2b6b729dafe249da965beef90590a
Jake Wang <wxx9248@qq.com>
2022-07-18 23:15:15 -0400

    Added Scrolling Fadeout Animation (#6)
    Signed-off-by: wxx9248 <wxx9248@qq.com>

--- AnywhereIceCream/src/main/webapp/js/index.js
+++ AnywhereIceCream/src/main/webapp/js/index.js
@@ -1,4 +1,20 @@
 $(document).ready(() => {
+    // Scrolling animation
+    $("#order-now").fadeTo(500, 1);
+    $(window).scroll(() => {
+        let windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".fade").each((_, element) => {
+            let objectBottom = $(element).offset().top + $(element).outerHeight();
+            if (objectBottom < windowBottom * 1.25) {
+                if ($(element).css("opacity") === "0") {
+                    $(element).fadeTo(500, 1);
+                }
+            }
+        });
+    });
+    $(window).scroll();
+
+    // Sign in & Sign up dialog
     let signModal = $("#nav-modal");
     let signTitle = $("#sign-title");
     let modalClose = $("#modal-close");
