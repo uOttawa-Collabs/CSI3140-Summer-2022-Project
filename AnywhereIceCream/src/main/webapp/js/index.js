0f72f4f102f11c0dba3be77137d4e1f1489b2047
wxx9248 <wxx9248@qq.com>
2022-07-18 22:54:06 -0400

    Added Scrolling Fadeout Animation
    Signed-off-by: wxx9248 <wxx9248@qq.com>

--- AnywhereIceCream/src/main/webapp/js/index.js
+++ AnywhereIceCream/src/main/webapp/js/index.js
@@ -1,2 +1,15 @@
-(() => {
-})();
+$(document).ready(() => {
+    $("#order-now").fadeTo(500, 1);
+    $(window).scroll(() => {
+        let windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".fade").each((_, element) => {
+            let objectBottom = $(element).offset().top + $(element).outerHeight();
+            if (objectBottom < windowBottom * 1.25) {
+                if ($(element).css("opacity") === "0") {
+                    $(element).fadeTo(500, 1);
+                }
+            }
+        });
+    });
+    $(window).scroll();
+});
