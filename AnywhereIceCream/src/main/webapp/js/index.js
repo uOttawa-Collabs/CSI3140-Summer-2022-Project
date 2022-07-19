6a777c5a65ac9355b2434c2ddcb6f66ff712207d
Xiayu Li <71458184+SayanoSong@users.noreply.github.com>
2022-07-18 22:02:09 -0400

    Change

--- AnywhereIceCream/src/main/webapp/js/index.js
+++ AnywhereIceCream/src/main/webapp/js/index.js
@@ -1,2 +1,39 @@
-(() => {
-})();
+
+function navBtn_Click(para) {
+    var signInModal = document.getElementById("navModal");
+    signInModal.style.display = "block";
+
+    if(para == "In"){
+        document.getElementById("signTitle").textContent = "Sign In";
+        document.getElementById("modalSubmit").textContent = "Sign In";
+    }else{
+        document.getElementById("signTitle").textContent = "Sign Up";
+        document.getElementById("modalSubmit").textContent = "Sign Up";
+        
+    }
+}
+
+function span_Click() {
+    var signInModal = document.getElementById("navModal");
+    signInModal.style.display = "none";
+}
+
+function SignIn_Click(){
+    var para = document.getElementById("signTitle").textContent;
+    if(para == "Sign In"){
+        alert("Sign In Successfully.");
+    }else{
+        alert("Sign Up Successfully.")
+    }
+    
+}
+
+window.onclick = function(event) {
+    var signInModal = document.getElementById("navModal");
+    if (event.target == signInModal) {
+        signInModal.style.display = "none";
+    }
+}
+
+
+
